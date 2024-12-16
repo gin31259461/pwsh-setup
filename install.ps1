035969b8d51603b6725b17e8f1bfcb40a2657e0f
Abner <qw0207060413@gmail.com>
2024-12-16 12:56:50 +0800

    edit install script: get fonts folder

--- install.ps1
+++ install.ps1
@@ -5,6 +5,8 @@ Copy-Item -Path ./.starship -Destination $HOME/ -Recurse -Force
 Copy-Item -Path ./.pwsh -Destination $HOME/ -Recurse -Force
 Copy-Item -Path ./files-theme-xcad.xaml -Destination $HOME/ -Recurse -Force
 Copy-Item -Path ./windows-terminal-settings.json -Destination $HOME/ -Recurse -Force
-Copy-Item -Path ./fira-code/* -Destination C:/Windows/Fonts/ -Recurse -Force
+
+$fontsPath = [System.Environment]::GetFolderPath("Fonts")
+Copy-Item -Path ./fira-code/* -Destination $fontsPath -Recurse -Force
 
 New-Item -Path $profile -Value $HOME/.pwsh/Microsoft.PowerShell_profile.ps1 -ItemType SymbolicLink -Force
