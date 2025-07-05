9d99f9ce8bb24760304229069e21333663bf2cd1
abner <qw0207060413@gmail.com>
2025-07-05 20:38:41 +0800

    fix: string error

--- install.ps1
+++ install.ps1
@@ -17,7 +17,7 @@ Copy-Item -Path ./windows-terminal-settings.json -Destination $HOME/ -Recurse -F
 
 Write-Output "Installing Fonts"
 
-$fontsPath = powershell -Command "[System.Environment]::GetFolderPath("Fonts")"
+$fontsPath = powershell -Command "[System.Environment]::GetFolderPath('Fonts')"
 Copy-Item -Path ./fonts/* -Destination $fontsPath -Recurse -Force
 
 Write-Output "Copying PowerShell Profile"
