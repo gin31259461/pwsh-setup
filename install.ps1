603e111279abf758ae3032ed97b622a85a378f5e
abner <qw0207060413@gmail.com>
2025-07-05 22:30:00 +0800

    remove: install font code because not working on windows

--- install.ps1
+++ install.ps1
@@ -11,37 +11,6 @@ Copy-Item -Path ./.pwsh -Destination $HOME/ -Recurse -Force
 
 New-Item -Path $profile -Value $HOME/.pwsh/Microsoft.PowerShell_profile.ps1 -ItemType SymbolicLink -Force
 
-# Fonts
-Write-Output "Installing Fonts"
-
-$fontSourceFolder = "./fonts"
-$SystemFontsPath = Join-Path $env:windir "Fonts"
-
-foreach($FontFile in Get-ChildItem $fontSourceFolder -Include '*.ttf','*.ttc','*.otf' -recurse ) {
-	$targetPath = Join-Path $SystemFontsPath $FontFile.Name
-
-	if(Test-Path -Path $targetPath){
-		$FontFile.Name + " already installed"
-	}
-
-	else {
-		Write-Output "Installing font " + $FontFile.Name
-		
-		#Extract Font information for Reqistry 
-		$ShellFolder = (New-Object -COMObject Shell.Application).Namespace($fontSourceFolder)
-		$ShellFile = $ShellFolder.ParseName($FontFile.name)
-		$ShellFileType = $ShellFolder.GetDetailsOf($ShellFile, 2)
-
-		#Set the $FontType Variable
-		If ($ShellFileType -Like '*TrueType font file*') {$FontType = '(TrueType)'}
-			
-		#Update Registry and copy font to font directory
-		$RegName = $ShellFolder.GetDetailsOf($ShellFile, 21) + ' ' + $FontType
-		New-ItemProperty -Name $RegName -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -PropertyType string -Value $FontFile.name -Force | out-null
-		Copy-item $FontFile.FullName -Destination $SystemFontsPath
-	}
-}
-
 # Packages
 Write-Output "Installing Packages"
 
