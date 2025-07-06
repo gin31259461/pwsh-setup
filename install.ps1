46642cff39ebe603809db7f09a18ddf96f8a42df
abner <qw0207060413@gmail.com>
2025-07-06 20:25:34 +0800

    add: win10, win11 right-click menu ui

--- install.ps1
+++ install.ps1
@@ -40,10 +40,4 @@ if (-not (winget list | Select-String -Pattern "NodeJS")) {
   npm install -g yarn
 }
 
-
-# Write-Output "Changing Right-Click Menu to Win10 Style"
-#
-# reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
-# taskkill /f /im explorer.exe; start explorer.exe
-
 Write-Output "All Done!"
