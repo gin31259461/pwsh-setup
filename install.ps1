2e71c0a88525b8a973e5e3d2f822eaae27fcb3d8
abnertu <qw0207060413@gmail.com>
2025-08-04 22:13:09 +0800

    chore: add check msg

--- install.ps1
+++ install.ps1
@@ -14,30 +14,41 @@ New-Item -Path $profile -Value $HOME/.pwsh/Microsoft.PowerShell_profile.ps1 -Ite
 # Packages
 Write-Output "Installing Packages"
 
+Write-Output "Check for PSReadLine"
 if (-not (Get-Module -ListAvailable -Name PSReadLine)) {
   Install-Module PSReadLine -Force
 }
+Write-Output "Done"
 
+Write-Output "Check for Starship"
 if (-not (winget list | Select-String -Pattern "Starship")) {
   winget install --id Starship.Starship
 }
+Write-Output "Done"
 
+Write-Output "Check for Neovim"
 if (-not (winget list | Select-String -Pattern "Neovim")) {
   winget install --id Neovim.Neovim
 }
+Write-Output "Done"
 
+Write-Output "Check for WezTerm"
 if (-not (winget list | Select-String -Pattern "WezTerm")) {
   winget install --id wez.wezterm
 }
+Write-Output "Done"
 
+Write-Output "Check for Alacritty"
 if (-not (winget list | Select-String -Pattern "Alacritty")) {
   winget install --id Alacritty.Alacritty 
 }
+Write-Output "Done"
 
+Write-Output "Check for NodeJS"
 if (-not (winget list | Select-String -Pattern "NodeJS")) {
   winget install --id OpenJS.NodeJS 
-  Get-ChildItem Env:
-  npm install -g yarn
+  npm install -g pnpm
 }
+Write-Output "Done"
 
 Write-Output "All Done!"
